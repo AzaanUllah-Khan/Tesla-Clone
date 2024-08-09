d55c11f04c2461cc3c41ff9ec4632d0b74e40195
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-08-10 02:23:10 +0500

    single commit #2

--- script.js
+++ script.js
@@ -20,13 +20,42 @@ window.addEventListener('scroll', function() {
 });
 
 const hoverItems = document.querySelectorAll('.links h1');
+const allH1 = document.querySelectorAll('.links h1');
+const allI = document.querySelectorAll('.links i');
 
 hoverItems.forEach(hoverItem => {
     hoverItem.addEventListener('mouseenter', () => {
         document.body.style.overflowY = 'hidden';
+        document.querySelector('.slider-top').style.top = '0';
+        document.querySelector('.blur').style.display = "block";
+    
+        const svgPaths = document.querySelectorAll('nav svg path');
+        svgPaths.forEach(path => {
+            path.style.fill = 'black';
+        });
+    
+        allH1.forEach(h1 => {
+            h1.style.color = 'black';
+        });
+    
+        allI.forEach(i => {
+            i.style.color = 'black';
+        });
     });
-
+    
     hoverItem.addEventListener('mouseleave', () => {
         document.body.style.overflowY = 'auto';
+        document.querySelector('.slider-top').style.top = '-100%';
+        document.querySelector('.blur').style.display = "none";
+        const svgPaths = document.querySelectorAll('nav svg path');
+        svgPaths.forEach(path => {
+            path.style.fill = 'white';
+        });
+        allH1.forEach(h1 => {
+            h1.style.color = 'white';
+        });
+        allI.forEach(i => {
+            i.style.color = 'white';
+        });
     });
 });
