af0a382a9b613262275f6d9562cc49ba6abebf01
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-08-09 18:34:34 +0500

    unnecessary commits removed

--- script.js
+++ script.js
@@ -5,19 +5,16 @@ window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     if (scrollTop > lastScrollTop) {
-        // Scrolling down
         navbar.classList.add('hidden');
     } else {
-        // Scrolling up
         navbar.classList.remove('hidden');
         navbar.classList.add('scrolled-up');
     }
 
-    // If the user scrolls back to the top
     if (scrollTop === 0) {
         navbar.classList.remove('scrolled-up');
         navbar.classList.remove('hidden');
     }
 
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Ensure lastScrollTop doesn't go below 0
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 });
